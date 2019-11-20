848dff53d231e61a9dcdedef4fbb692ead8559ff
Koichi Taura <taura.koichi@gmail.com>
2019-11-20 23:01:42 +0900

    add home and detail components

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>TODO List</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
+import Board from './components/Board.vue';
 
 Vue.use(Router)
 
 const routes = [
-    { path: '/', component: HelloWorld },
+    { path: '/', component: Home },
+    { path: '/board', component: Board },
 ];
 
 const router = new Router({
