22333ac423f4c89632ce0c7216724d2215de47cd
Koichi Taura <taura.koichi@gmail.com>
2019-11-22 00:31:09 +0900

    add home card ui

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,35 @@
     <div>
         <h1>TODO List</h1>
         <div>
-            <ul>
-                <li>
-                    <button class="btn btn-primary" text="aaa" />
-                </li>
-                <li>
-                    <font-awesome-icon icon="coffee" />
-                </li>
-            </ul>
+            <div class="row">
+                <div class="col-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">TESTボード</h5>
+                        <p class="card-text">
+                             これはTESTボードで試しに作ってみました。
+                        </p>
+                    </div>
+                </div>
+                <div class="col-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">TESTボード2</h5>
+                        <p class="card-text">
+                             これはTESTボードで試しに作ってみました。
+                        </p>
+                    </div>
+                </div>
+                <div class="col-3 card">
+                    <div class="card-body">
+                        <h5 class="card-title">TESTボード3</h5>
+                        <p class="card-text">
+                             これはTESTボードで試しに作ってみました。
+                        </p>
+                    </div>
+                </div>
+                <a class="btn btn-primary col-3" href="#">
+                    <font-awesome-icon style="height: 100px;" class="align-middle" icon="plus" />
+                </a>
+            </div>
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoffee)
+library.add(faCoffee, faPlus)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue)
 
