556488f39c2781ec418b7623b16146f132b4e52b
Koichi Taura <taura.koichi@gmail.com>
2019-11-22 00:05:06 +0900

    add bootstrap and awesomefont

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <h1>TODO List</h1>
-
+        <div>
+            <ul>
+                <li>
+                    <button class="btn btn-primary" text="aaa" />
+                </li>
+                <li>
+                    <font-awesome-icon icon="coffee" />
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
