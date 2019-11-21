0d302953c61b9d32446561fc3a4290804aaa9e91
Koichi Taura <taura.koichi@gmail.com>
2019-11-22 03:04:15 +0900

    add data with vue data function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,33 +3,20 @@
         <h1>TODO List</h1>
         <div>
             <div class="row">
-                <div class="col-3 card">
+                <div v-for="(thread, index) in threads" :key="index" class="col-3 card" style="position: relative;">
+                    <span class="col-12 text-right" style="position: absolute; right: -15px">
+                        <font-awesome-icon icon="times-circle" />
+                    </span>
                     <div class="card-body">
-                        <h5 class="card-title">TESTボード</h5>
+                        <h5 class="card-title">{{ thread.title }}</h5>
                         <p class="card-text">
-                             これはTESTボードで試しに作ってみました。
+                             {{ thread.description }}
                         </p>
                     </div>
                 </div>
-                <div class="col-3 card">
-                    <div class="card-body">
-                        <h5 class="card-title">TESTボード2</h5>
-                        <p class="card-text">
-                             これはTESTボードで試しに作ってみました。
-                        </p>
-                    </div>
-                </div>
-                <div class="col-3 card">
-                    <div class="card-body">
-                        <h5 class="card-title">TESTボード3</h5>
-                        <p class="card-text">
-                             これはTESTボードで試しに作ってみました。
-                        </p>
-                    </div>
-                </div>
-                <a class="btn btn-primary col-3" href="#">
+                <button class="btn btn-primary col-3" href="#">
                     <font-awesome-icon style="height: 100px;" class="align-middle" icon="plus" />
-                </a>
+                </button>
             </div>
         </div>
     </div>
@@ -37,6 +24,20 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data: function() {
+      return {
+          threads: [
+              {title: 'TESTボード', description: 'これはTESTボードで試しに作ってみました。'},
+              {title: 'TESTボード2', description: 'これはTESTボードで試しに作ってみました。'},
+              {title: 'TESTボード3', description: 'これはTESTボードで試しに作ってみました。'}
+          ]
+      }
+  },
+  methods: {
+      createBoard: function() {
+
+      }
+  }
 }
 </script>
