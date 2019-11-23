c1b60b94afa7b1d937cf2a74cbec1952c21060b6
Koichi Taura <taura.koichi@gmail.com>
2019-11-23 15:54:04 +0900

    modify design of home board

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <h1>TODO List</h1>
-        <div>
+        <div style="margin: 30px;">
             <div class="row">
-                <div v-for="(thread, index) in threads" :key="index" class="col-3 card" style="position: relative;">
-                    <span v-on:click="deleteCard(index)" class="col-12 text-right" style="position: absolute; right: -15px">
+                <div v-for="(thread, index) in threads" :key="index" class="col-3 card" style="margin-bottom: 10px; position: relative;">
+                    <div class="">
+                    <span v-on:click="deleteCard(index)" class="col-12 text-right" style="position: absolute; right: -5px">
                         <font-awesome-icon icon="times-circle" />
                     </span>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ thread.title }}</h5>
-                        <p class="card-text">
-                             {{ thread.description }}
-                        </p>
+                    <a class="" v-bind:href="'/board/' + index" >
+                        <div class="card-body">
+                            <h5 class="card-title">{{ thread.title }}</h5>
+                            <p class="card-text">
+                                 {{ thread.description }}
+                            </p>
+                        </div>
+                    </a>
                     </div>
                 </div>
                 <div v-if="createFlag" class="col-3 card">
@@ -23,7 +27,7 @@
                         Create
                     </button>
                 </div>
-                <button v-else v-on:click="openCreateForm" class="btn btn-primary col-3" href="#">
+                <button  v-else v-on:click="openCreateForm" class="col-3 btn btn-primary" href="#">
                     <font-awesome-icon style="height: 100px;" class="align-middle" icon="plus" />
                 </button>
             </div>
